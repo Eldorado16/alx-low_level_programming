d5794a607636ff1ce1f727668d6da9c7ee9d8198
Eldorado16 <65019139+Eldorado16@users.noreply.github.com>
2021-09-01 08:07:29 -0800

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,17 +1,22 @@
 #include "holberton.h"
 
 /**
- * print_alphabet - print the alphabet
- *
- * Description: print the alphabet, in lowercase, followed by a new line
- * Return: void
- */
-
-void print_alphabet(void)
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
 {
-	int c;
-
-	for (c = 'a'; c <= 'z'; ++c)
-		_putchar(c);
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
+	return (0);
 }
