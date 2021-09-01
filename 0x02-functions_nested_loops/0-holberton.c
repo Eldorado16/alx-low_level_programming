7ca82f1e211ae0cff0da220b55dc89fc3b89036d
Eldorado16 <65019139+Eldorado16@users.noreply.github.com>
2021-09-01 08:30:00 -0800

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,22 +1,21 @@
 #include "main.h"
 /**
- * main - entry point
- *
- * Return: On success 0.
- */
-/**
- * main - prints the string "Holberton" from a character array.
- * Return: 0 on success.
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	char c[] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
 }
