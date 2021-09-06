140fb891241abdc493bd04de7e48c830b15f13e2
Eldorado16 <65019139+Eldorado16@users.noreply.github.com>
2021-09-05 22:26:53 -0800

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,21 @@
 #include "main.h"
+
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+    puts2(str);
+    str = "666";
+    puts2(str);
+    str = "#";
+    puts2(str);
+    str = "";
+    puts2(str);
+    return (0);
+}
+
+
 void puts2(char *str)
 {
 	if (str)
